ca1b314aa68e2a080611d4d9efb6882c5d06525e
magh <manuel.gatica.holtmann@gmail.com>
2019-03-10 09:28:27 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace commonBoss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
